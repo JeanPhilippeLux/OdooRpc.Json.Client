20be62138cbd8418d37d8846c4d0d7073f0a5937
JeanPhilippeLux <91731146+JeanPhilippeLux@users.noreply.github.com>
2025-02-17 15:16:08 +0100

    Add method to simplify the creation of lines with a header.

---
+++ OdooRpc.Json.Client/Models/OdooLinesStructure.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdooRpc.Json.Client.Models
+{
+    public class OdooLinesStructure : List<object>
+    {
+
+        /// <summary>
+        /// Add element to list of lines used in Odoo structure to add lines with a header in RPC call.
+        /// </summary>
+        /// <param name="odooLinesCommands"></param>
+        /// <param name="odooRecordId">Id of record used for the commands Update, Delete, Unlink and Link. Must be O for commands Create, Clear and Set</param>
+        /// <param name="odooDictionaryModel">Record to add in list</param>
+        public void AddElement(OdooLinesCommands odooLinesCommands, long odooRecordId, OdooDictionaryModel odooDictionaryModel)
+        {
+            this.Add(new object[] { (int)odooLinesCommands, odooRecordId, odooDictionaryModel.GetRecord() }.ToArray());
+        }
+
+    }
+
+
+    public enum OdooLinesCommands
+    {
+        Create = 0,
+        Update = 1,
+        Delete = 2,
+        Unlink = 3,
+        Link = 4,
+        Clear = 5,
+        Set = 6
+    }
+}
