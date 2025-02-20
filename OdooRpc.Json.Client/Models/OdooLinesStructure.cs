c9ba0b9cf8f221c4fb8a9eb126d23cad76f5f7fd
JeanPhilippeLux <91731146+JeanPhilippeLux@users.noreply.github.com>
2025-02-20 16:23:50 +0100

    Add possibility to add list of integers in OdooLinesStructures.

--- OdooRpc.Json.Client/Models/OdooLinesStructure.cs
+++ OdooRpc.Json.Client/Models/OdooLinesStructure.cs
@@ -12,12 +12,23 @@ namespace OdooRpc.Json.Client.Models
         /// <summary>
         /// Add element to list of lines used in Odoo structure to add lines with a header in RPC call.
         /// </summary>
-        /// <param name="odooLinesCommands"></param>
-        /// <param name="odooRecordId">Id of record used for the commands Update, Delete, Unlink and Link. Must be O for commands Create, Clear and Set</param>
-        /// <param name="odooDictionaryModel">Record to add in list</param>
+        /// <param name="odooLinesCommands">Command of the record.</param>
+        /// <param name="odooRecordId">Id of record used for the commands Update, Delete, Unlink and Link. Must be O for commands Create, Clear and Set.</param>
+        /// <param name="odooDictionaryModel">Record to add in list.</param>
         public void AddElement(OdooLinesCommands odooLinesCommands, long odooRecordId, OdooDictionaryModel odooDictionaryModel)
         {
             this.Add(new object[] { (int)odooLinesCommands, odooRecordId, odooDictionaryModel.GetRecord() }.ToArray());
+        }
+
+        /// <summary>
+        /// Add element to list of lines used in Odoo structure to add lines with a header in RPC call.
+        /// </summary>
+        /// <param name="odooLinesCommands">Command of the record.</param>
+        /// <param name="odooRecordId">Id of record used for the commands Update, Delete, Unlink and Link. Must be O for commands Create, Clear and Set.</param>
+        /// <param name="value">List of integer to add directly in the list.</param>
+        public void AddElement(OdooLinesCommands odooLinesCommands, long odooRecordId, List<int> value)
+        {
+            this.Add(new object[] { (int)odooLinesCommands, odooRecordId, value }.ToArray());
         }
 
     }
