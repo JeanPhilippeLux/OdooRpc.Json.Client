198d9a90ccca40ca1f6669b681b594aad7b89a41
JeanPhilippeLux <91731146+JeanPhilippeLux@users.noreply.github.com>
2025-03-04 14:12:02 +0100

    Add ProductTemplateAttributeValue Model

---
+++ OdooRpc.Json.Client.Shared/Models/ProductTemplateAttributeValueOdoo.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using OdooRpc.Json.Client.Attributes;
+using OdooRpc.Json.Client.Converters;
+using OdooRpc.Json.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdooRpc.Json.Client.Shared.Models
+{
+
+    
+    [OdooTableName("product.template.attribute.value")]
+    [JsonConverter(typeof(OdooModelConverter))]
+    public class ProductTemplateAttributeValueOdoo : IOdooModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+        [JsonProperty("attribute_id")]
+        public long AttributeId { get; set; }
+        [JsonProperty("attribute_line_id")]
+        public long AttributeLineId { get; set; }
+        [JsonProperty("product_attribute_value_id")]
+        public long ProductAttributeValueId { get; set; }
+
+
+    }
+}
