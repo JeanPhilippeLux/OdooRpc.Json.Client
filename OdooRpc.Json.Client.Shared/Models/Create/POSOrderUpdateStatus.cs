45b968c12def018dcf46e5a2db9459113d5049ee
JeanPhilippeLux <91731146+JeanPhilippeLux@users.noreply.github.com>
2025-03-14 09:01:19 +0100

    Add POS.Order update state in Odoo create model.

---
+++ OdooRpc.Json.Client.Shared/Models/Create/POSOrderUpdateStatus.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using OdooRpc.Json.Client.Attributes;
+using OdooRpc.Json.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdooRpc.Json.Client.Shared.Models.Create
+{
+    [OdooTableName("pos.order")]
+    public class POSOrderUpdateStatus : IOdooCreateModel
+    {
+        [JsonProperty("state")]
+        public POSStatusOdooEnum? State { get; set; }
+    }
+}
