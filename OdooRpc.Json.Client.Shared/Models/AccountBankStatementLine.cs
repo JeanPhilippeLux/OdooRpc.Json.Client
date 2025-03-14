7891c138b2ed23c8c401e5bfcc353d1a24cf4b57
JeanPhilippeLux <91731146+JeanPhilippeLux@users.noreply.github.com>
2025-03-14 09:00:46 +0100

    Add POS.Payment model, Account.Bank.Statement line

---
+++ OdooRpc.Json.Client.Shared/Models/AccountBankStatementLine.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using OdooRpc.Json.Client.Attributes;
+using OdooRpc.Json.Client.Converters;
+using OdooRpc.Json.Client.Models;
+using System.Runtime.Serialization;
+
+
+namespace OdooRpc.Json.Client.Shared.Models
+{
+    [OdooTableName("account.bank.statement.line")]
+    [JsonConverter(typeof(OdooModelConverter))]
+
+    public class AccountBankStatementLineOdooModel : IOdooModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+        [JsonProperty("pos_session_id")]
+        public long POSSessionId { get; set; }
+        [JsonProperty("journal_id")]
+        public long JournalId { get; set; }
+
+        [JsonProperty("amount")]
+        public decimal Amount { get; set; }
+
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+        [JsonProperty("payment_ref")]
+        public string PaymentRef { get; set; }
+        
+        
+    }
+}
---
+++ OdooRpc.Json.Client.Shared/Models/POSPaymentOdooModel.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using OdooRpc.Json.Client.Attributes;
+using OdooRpc.Json.Client.Converters;
+using OdooRpc.Json.Client.Models;
+using System.Runtime.Serialization;
+
+namespace OdooRpc.Json.Client.Shared.Models
+{
+    
+    [OdooTableName("pos.payment")]
+    [JsonConverter(typeof(OdooModelConverter))]
+    public class POSPaymentOdooModel : IOdooModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("amount")]
+        public double? Amount { get; set; }
+        [JsonProperty("payment_date")]
+        public DateTime PaymentDate { get; set; }
+        [JsonProperty("payment_method_id")]
+        public long PaymentMethodId { get; set; }
+
+        [JsonProperty("pos_order_id")]
+        public long POSOrderId { get; set; }
+
+
+    }
+
+    
+
+}
