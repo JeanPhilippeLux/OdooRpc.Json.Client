9d5959c883c1e9928c1aa87ab7887700448bedde
JeanPhilippeLux <91731146+JeanPhilippeLux@users.noreply.github.com>
2025-04-30 17:11:31 +0200

    Add pos.close.session.wizard model

---
+++ OdooRpc.Json.Client.Shared/Models/PosCloseSessionWizardOdooModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using OdooRpc.Json.Client.Attributes;
+using OdooRpc.Json.Client.Converters;
+using OdooRpc.Json.Client.Models;
+using System.Runtime.Serialization;
+
+namespace OdooRpc.Json.Client.Shared.Models
+{
+    [OdooTableName("pos.close.session.wizard")]
+    [JsonConverter(typeof(OdooModelConverter))]
+    public class PosCloseSessionWizardOdooModel : IOdooModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+    }
+}
