c43910d50f597ad3a2658611c81c56244eb65924
JeanPhilippeLux <91731146+JeanPhilippeLux@users.noreply.github.com>
2025-05-06 15:41:31 +0200

    Update creation model and adding PosCloseSessionWizard update model

---
+++ OdooRpc.Json.Client.Shared/Models/Create/PosCloseSessionWizardUpdateAccountId.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using OdooRpc.Json.Client.Attributes;
+using OdooRpc.Json.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdooRpc.Json.Client.Shared.Models.Create
+{
+    [OdooTableName("pos.close.session.wizard")]
+    public class PosCloseSessionWizardUpdateAccountId : IOdooCreateModel
+    {
+        [JsonProperty("account_id")]
+        public long account_id { get; set; }
+    }
+}
