4660501ef32dca6fd268d75204cbe3f16691cd66
JeanPhilippeLux <91731146+JeanPhilippeLux@users.noreply.github.com>
2025-06-03 16:31:55 +0200

    Add Update Data Order on Sale.Order creation models.

---
+++ OdooRpc.Json.Client.Shared/Models/Create/OdooUpdateDateOrderSalesOrder.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using OdooRpc.Json.Client.Attributes;
+using OdooRpc.Json.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdooRpc.Json.Client.Shared.Models.Create
+{
+    [OdooTableName("sale.order")]
+    public class OdooUpdateDateOrderSalesOrder : IOdooCreateModel
+    {
+        [JsonProperty("date_order")]
+        public DateTime DateOrder { get; set; }
+    }
+}
